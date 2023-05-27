d67343e38fb5940e366658b7f0ec3070aeff2e30
ooijingkai <ooijingkai@gmail.com>
2023-05-28 01:50:01 +0800

    feat: dotfile repo

---
+++ nvim/init.lua
@@ -0,0 +1,8 @@
+require("kai.plugins-setup")
+require("kai.core.options")
+require("kai.core.keymaps")
+require("kai.core.colorscheme")
+require("kai.plugins.comment")
+require("kai.plugins.nvim-tree")
+require("kai.plugins.lualine")
+require("kai.plugins.telescope")
