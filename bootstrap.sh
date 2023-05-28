d41fb5e0a62569e72f4c2cfb362bb75d65c3c0aa
ooijingkai <ooijingkai@gmail.com>
2023-05-28 23:29:03 +0800

    feat: Add link step in bootstrap file

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,14 @@
-# installs homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# # installs homebrew
+# /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+#
+# # install dev apps using homebrew
+# brew bundle
 
-# install dev apps using homebrew
-brew bundle
+# create .config dir
+mkdir ~/.config
+
+# set links
+DIR=$(pwd)
+
+ln -s $DIR/.zshrc ~/.zshrc
+ln -s $DIR/nvim ~/.config/nvim
